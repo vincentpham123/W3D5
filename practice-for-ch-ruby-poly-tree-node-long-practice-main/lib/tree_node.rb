869567de3c45569de0d1c768b66ff1748a7979a0
vincentpham123 <vincentpham32@gmail.com>
2023-04-14 15:30:10 -0700

    first part complete

--- practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb
+++ practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb
@@ -1,3 +1,42 @@
 class PolyTreeNode
-  
+    attr_reader :value, :parent, :children
+  def initialize(value, parent = nil)
+    @value = value
+    @parent = parent
+    @children = []
+  end
+
+  # for children, change @parent to the passed node
+  # for the parent, add child to @children, p
+
+  def parent=(new_parent)
+        if new_parent == nil 
+            @parent = nil 
+        else
+            if @parent == nil 
+                @parent = new_parent
+                new_parent.children << self if !new_parent.children.include?(self)
+            else
+                @parent.children.delete(self)
+                @parent = new_parent
+                new_parent.children << self if !new_parent.children.include?(self)
+            end 
+        end
+    end
+
+    def add_child(child)
+        child.parent = self 
+    end
+
+    def remove_child(child)
+        
+        if child.parent.children.include?(child)
+            child.parent = nil 
+        else
+            raise 'Not a child'
+        end
+    endgit
 end
+
+# Part 2
+
